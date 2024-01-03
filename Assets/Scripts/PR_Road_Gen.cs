cc639c871c68f52f98e35e4a9e865a693318417a
GoldGumer <ngonczol@gmail.com>
2024-01-03 18:59:29 +0000

    started working with splines

--- Assets/Scripts/PR_Road_Gen.cs
+++ Assets/Scripts/PR_Road_Gen.cs
@@ -1,59 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Splines;
 
 public class PR_Road_Gen : MonoBehaviour
 {
     [SerializeField] GameObject roadShape;
-    [SerializeField] int bezierSegments;
-    static int arraySize = 6;
-    GameObject[] roadPoints = new GameObject[arraySize];
+    [SerializeField] int splineLength;
+    GameObject roadSpline;
 
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < arraySize; i++)
+        roadSpline = new GameObject("Road Spline");
+        roadSpline.transform.parent = transform;
+        roadSpline.AddComponent<SplineContainer>();
+        for (int i = 0; i < splineLength; i++)
         {
-            roadPoints[i] = new GameObject("Road point " + i);
-            roadPoints[i].transform.parent = transform;
-            roadPoints[i].transform.position = Vector3.forward * i * 10;
-            Instantiate(roadShape, roadPoints[i].transform.position, roadPoints[i].transform.rotation, roadPoints[i].transform);
+            roadSpline.GetComponent<SplineContainer>().Spline.Add(new BezierKnot(Vector3.forward * i * 10));
         }
-        
-    }
-
-    Vector3[] Bezier(Transform a, Transform b, int numPoints)
-    {
-        Vector3[] points = new Vector3[numPoints + 1];
-        Vector3 c = a.position + a.forward * 5;
-        for (int i = 0; i <= numPoints; i++)
-        {
-            float t = (i / (float)numPoints);
-            points[i] = Mathf.Pow(1 - t, 2) * a.position + 2 * (1 - t) * t * c + Mathf.Pow(t, 2) * b.position;
-        }
-        return points;
+        roadSpline.GetComponent<SplineContainer>().Spline.SetTangentMode(TangentMode.AutoSmooth);
     }
 
     private void OnDrawGizmos()
     {
-        if (roadPoints[0] != null)
+        if (roadSpline != null)
         {
-            
-            for (int i = 0; i < arraySize - 1; i++)
+            for (float i = 0; i < 1; i += 0.001f)
             {
-                Vector3[] points = Bezier(roadPoints[i].transform, roadPoints[i + 1].transform, bezierSegments);
-                for (int j = 0; j < bezierSegments; j++)
+                float3 pos1, tang1, up1, pos2, tang2, up2;
+                roadSpline.GetComponent<SplineContainer>().Evaluate(i, out pos1, out tang1, out up1);
+                roadSpline.GetComponent<SplineContainer>().Evaluate(i + 0.001f, out pos2, out tang2, out up2);
+                foreach (var knot in roadShape.GetComponent<SplineContainer>().Spline.Knots)
                 {
-                    Gizmos.DrawLine(points[j], points[j + 1]);
+                    Gizmos.DrawLine(pos1 + knot.Position, pos2 + knot.Position);
                 }
             }
-            //    for (int j = 0; j < roadShape.transform.childCount; j++)
-            //    {
-            //        Gizmos.color = Color.magenta;
-            //        Transform currentRoadShape = roadPoints[i].transform.GetChild(0);
-            //        Transform nextRoadShape = roadPoints[i + 1].transform.GetChild(0);
-            //        Gizmos.DrawLine(currentRoadShape.GetChild(j).transform.position, nextRoadShape.GetChild(j).transform.position);
-            //    }
         }
     }
 }
