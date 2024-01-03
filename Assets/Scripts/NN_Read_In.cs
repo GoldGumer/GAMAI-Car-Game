1e12a9b364ab100cb4162bcd0522852dccad016c
GoldGumer <ngonczol@gmail.com>
2024-01-03 09:34:56 +0000

    init

---
+++ Assets/Scripts/NN_Read_In.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NN_Read_In : MonoBehaviour
+{
+    ComputeShader carBrain;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
